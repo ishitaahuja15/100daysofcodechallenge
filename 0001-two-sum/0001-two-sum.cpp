2320c832292e0dcafc98742caf1e9fca59fb3ebb
Ishita Ahuja <95916507+ishitaahuja15@users.noreply.github.com>
2023-06-30 13:15:54 +0530

    Time: 513 ms (32.61%), Space: 10 MB (99.95%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i = 0;i<nums.size();i++){
+            for(int y=i+1;y<nums.size();y++){
+                if(nums[i]+nums[y]==target){
+                    return{i,y};
+                }
+                
+            }
+        }
+        return{};
+    }
+};
