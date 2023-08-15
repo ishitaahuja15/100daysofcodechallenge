e69149b694c56d37d6491932a32d376038523570
Ishita Ahuja <95916507+ishitaahuja15@users.noreply.github.com>
2023-08-15 09:32:44 +0530

    Time: 8 ms (38.73%), Space: 10.2 MB (58.98%) - LeetHub

---
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int count = 0;
+        for(int i = 0; i<nums.size();i++){
+            if(nums[i]<0){
+                count++;
+            }
+            else if(nums[i]==0){
+                return 0;
+            }
+        }
+              
+
+        if (count%2==0){
+            return 1;
+        }    
+        else if(count%2!=0){
+            return -1;
+        }
+        else{
+            return 0;
+        }
+    }
+};
