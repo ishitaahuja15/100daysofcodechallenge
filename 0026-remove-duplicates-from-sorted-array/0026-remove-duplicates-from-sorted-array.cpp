05b5831a090bb9b3a6b18cf3ed3f4e33d673bc76
Ishita Ahuja <95916507+ishitaahuja15@users.noreply.github.com>
2023-10-04 11:48:51 +0530

    Time: 11 ms (42.71%), Space: 18.9 MB (17.78%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-      int counter =1;
-      for(int i = 0; i<nums.size()-1;i++){
-        if(nums[i]!=nums[i+1]){
-          nums[counter]= nums[i+1];
-          counter++;
-        } 
-        
-      }
-      return counter;
+        int counter = 1;
+        for(int i =0; i<nums.size()-1;i++){
+            if(nums[i]!=nums[i+1]){
+                nums[counter]=nums[i+1];
+                counter++;
+            }
+        }
+        return counter;
     }
 };
