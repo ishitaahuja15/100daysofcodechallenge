0b98b9ce10256637eb1f12da3820976ec0fc8711
Ishita Ahuja <95916507+ishitaahuja15@users.noreply.github.com>
2023-10-17 13:58:08 +0530

    Time: 13 ms (7.15%), Space: 20.9 MB (13.66%) - LeetHub

--- 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.cpp
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
-        int n = hours.size();
         int count =0;
-        for(int i =0; i<n;i++){
+        for(int i =0; i<hours.size();i++){
             if( hours[i]>=target){
                 count++;
             }
