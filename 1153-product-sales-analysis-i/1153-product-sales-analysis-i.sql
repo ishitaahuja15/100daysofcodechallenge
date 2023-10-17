e5b464686dab99a8a9de85c27746bb5e007d673e
Ishita Ahuja <95916507+ishitaahuja15@users.noreply.github.com>
2023-10-17 12:59:10 +0530

    Time: 1732 ms (89.6%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 SELECT DISTINCT product_name, year, price FROM Sales LEFT JOIN Product 
-ON Sales.product_id = product.product_id;
+ON Sales.product_id = product.product_id ORDER BY product_name;
